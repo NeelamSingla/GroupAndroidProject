ded9995afac67a7c68d82353f85244995a9b8195
Pradeep Agrawal <pradeepagrawal53@gmail.com>
2018-04-26 14:35:23 +0200

    Added Instructions activity screen

---
+++ app/src/main/java/com/jailbreackers/soulhunter/HelpInstructionsActivity.kt
@@ -0,0 +1,12 @@
+package com.jailbreackers.soulhunter
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HelpInstructionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_help_instructions)
+    }
+}
--- app/src/main/java/com/jailbreackers/soulhunter/HomeMenuActivity.kt
+++ app/src/main/java/com/jailbreackers/soulhunter/HomeMenuActivity.kt
@@ -24,8 +24,4 @@ class HomeMenuActivity : AppCompatActivity() {
         }
     }
 
-
-
-
-
 }
