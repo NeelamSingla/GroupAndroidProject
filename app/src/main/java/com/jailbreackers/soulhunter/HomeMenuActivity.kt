beebc1bb00f4f9041bc44787247047e49e4f8b86
tmp-sda-1160 <neelam.cse129@gmail.com>
2018-04-26 11:28:15 +0200

    Added Empty Activity for Instructions

--- app/src/main/java/com/jailbreackers/soulhunter/HomeMenuActivity.kt
+++ app/src/main/java/com/jailbreackers/soulhunter/HomeMenuActivity.kt
@@ -7,6 +7,7 @@ import kotlinx.android.synthetic.main.activity_home_menu.*
 
 class HomeMenuActivity : AppCompatActivity() {
     private val ADD_MAP = 1
+    private val ADD_HELP = 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_menu)
@@ -14,6 +15,12 @@ class HomeMenuActivity : AppCompatActivity() {
         {
             val intent = Intent(this, MapsActivity::class.java)
             startActivityForResult(intent, ADD_MAP)
+        }
+
+        fab_helpbutton.setOnClickListener(){
+
+            val intent = Intent(this, HelpInstructionsActivity::class.java)
+            startActivityForResult(intent, ADD_HELP)
         }
     }
 
