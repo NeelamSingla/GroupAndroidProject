dbb3965ad419143fbd13a503b70a4a5923a5a9f9
tmp-sda-1166 <mohamad.y.s.e@gmail.com>
2018-05-09 00:03:45 +0200

    added creating coins while catching

--- app/src/main/java/com/jailbreackers/soulhunter/MapsActivity.kt
+++ app/src/main/java/com/jailbreackers/soulhunter/MapsActivity.kt
@@ -301,6 +301,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 scoreLabel!!.setText(" Score : ${score} ")
                 coins[i].changeLocation(location, generate())
                 coins[i].generateValue()
+                Toast.makeText(this@MapsActivity," congratulations!!! you got ${coins[i].value!!} $ " , Toast.LENGTH_LONG).show()
 
                 // remove from arraylist
 
