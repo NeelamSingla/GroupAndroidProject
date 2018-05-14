fa011a1d1e895c360da6c3f563180d0c642181c9
qaisarm <qmphy99@gmail.com>
2018-05-14 12:45:20 +0200

    Updated the bestscore activity and split the array of display

--- app/src/main/java/com/jailbreackers/soulhunter/ScoreActivity.kt
+++ app/src/main/java/com/jailbreackers/soulhunter/ScoreActivity.kt
@@ -8,10 +8,13 @@ import android.widget.TextView
 
 class ScoreActivity : AppCompatActivity() {
 
-    internal var game_score: TextView? = null
-    internal var lastScore: Int = 0
-    internal var lastDistance: Int = 0
-    internal var lastCalories: Int = 0
+    internal var current_score: TextView? = null
+    internal var best1_score: TextView? = null
+    internal var best2_score: TextView? = null
+    internal var best3_score: TextView? = null
+    internal var currentScore: Int = 0
+    internal var currentDistance: Int = 0
+    internal var currentCalories: Int = 0
     internal var best1: Int = 0
     internal var best2: Int = 0
     internal var best3: Int = 0
@@ -19,49 +22,53 @@ class ScoreActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_score)
-        game_score = findViewById<View>(R.id.my_score) as TextView?
+        current_score = findViewById<View>(R.id.current_score) as TextView?
+        best1_score = findViewById<View>(R.id.best1) as TextView?
+        best2_score = findViewById<View>(R.id.best2) as TextView?
+        best3_score = findViewById<View>(R.id.best3) as TextView?
         val preferences = getSharedPreferences("PREFS", 0)
-        lastScore = preferences.getInt("lastScore", 0)
-        lastDistance= preferences.getInt("lastDistance", 0)
-        lastCalories= preferences.getInt("lastCalories", 0)
+        currentScore = preferences.getInt("currentScore", 0)
+        currentDistance= preferences.getInt("currentDistance", 0)
+        currentCalories= preferences.getInt("currentCalories", 0)
         best1 = preferences.getInt("best1", 0)
         best2 = preferences.getInt("best2", 0)
         best3 = preferences.getInt("best3", 0)
-        if (lastScore > best3) {
+        if (currentScore > best3) {
 
-            best3 = lastScore
+            best3 = currentScore
             val editor = preferences.edit()
             editor.putInt("best3", best3)
-            editor.putInt("lastCalories", lastCalories)
-            editor.putInt("lastDistance", lastDistance)
+            editor.putInt("currentCalories", currentCalories)
+            editor.putInt("currentDistance", currentDistance)
             editor.apply()
         }
-        if (lastScore > best2) {
+        if (currentScore > best2) {
             val temp = best2
-            best2 = lastScore
+            best2 = currentScore
             best3 = temp
             val editor = preferences.edit()
             editor.putInt("best3", best3)
             editor.putInt("best2", best2)
-            editor.putInt("lastCalories", lastCalories)
-            editor.putInt("lastDistance", lastDistance)
+            editor.putInt("currentCalories", currentCalories)
+            editor.putInt("currentDistance", currentDistance)
             editor.apply()
         }
-        if (lastScore > best1) {
+        if (currentScore > best1) {
             val temp = best1
-            best1 = lastScore
+            best1 = currentScore
             best2 = temp
             val editor = preferences.edit()
             editor.putInt("best2", best2)
             editor.putInt("best1", best1)
-            editor.putInt("lastCalories", lastCalories)
-            editor.putInt("lastDistance", lastDistance)
+            editor.putInt("currentCalories", currentCalories)
+            editor.putInt("currentDistance", currentDistance)
             editor.apply()
         }
-        game_score !!.text = "Last Score: " + lastScore + ", " + "Calories:" + lastCalories + ", " +  "Distance:"+ lastDistance + "\n" +
-                "Best1: " + best1 + ", " + "Calories:" + lastCalories+ ", " + "Distance:"+ lastDistance +"\n" +
-                "Best2: " + best2 + ", " + "Calories:" + lastCalories+ ", " + "Distance:"+ lastDistance +"\n" +
-                "Best3: " + best3 + ", " + "Calories:" + lastCalories+ ", " + "Distance:"+ lastDistance +"\n"
+        current_score !!.text = "Current Score: " + currentScore + ", " + "Calories:" + currentCalories + ", " +  "Distance:"+ currentDistance
+
+        best1_score !!.text = "Best1: " + best1 + ", " + "Calories:" + currentCalories+ ", " + "Distance:"+ currentDistance
+        best2_score !!.text = "Best2: " + best2 + ", " + "Calories:" + currentCalories+ ", " + "Distance:"+ currentDistance
+        best3_score !!.text = "Best3: " + best3 + ", " + "Calories:" + currentCalories+ ", " + "Distance:"+ currentDistance
     }
     override
     fun onBackPressed(){
